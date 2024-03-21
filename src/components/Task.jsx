c65f4714aff75d7f9b22774f0bcb52262cfb1686
srinivasGokarla <srinivasgokarla@gmail.com>
2024-03-21 18:32:53 +0530

    Done

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,10 +2,16 @@ import React, { useState, useEffect } from "react";
 
 const TaskList = ({ tasks }) => {
   const [taskList, setTaskList] = useState([]);
-
+  const [showForm, setShowForm] = useState(false); 
+  const [newCardData, setNewCardData] = useState({ time: "",  color : "" });
+  const [newCardStatus, setNewCardStatus] = useState("");
   useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem("tasks")) || tasks;
     setTaskList(tasks);
   }, [tasks]);
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+  }, [taskList]);
 
   const onDragStart = (evt) => {
     let element = evt.currentTarget;
@@ -52,6 +58,27 @@ const TaskList = ({ tasks }) => {
     });
     setTaskList(updated);
   };
+  const addCard = (status) => {
+    setShowForm(true);
+    setNewCardStatus(status);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const newCard = {
+      id: Math.floor(Math.random() * 1000), 
+      status: newCardStatus,
+      color: newCardData.color,
+      ...newCardData,
+     
+    
+    };
+    const updatedTasks = [...taskList, newCard];
+    setTaskList(updatedTasks);
+    setShowForm(false);
+    setNewCardData({ time: "",  color : "" });
+    alert("Added Successfully"); 
+  };
 
   let pending = taskList.filter((data) => data.status === "In Progress");
   let done = taskList.filter((data) => data.status === "Completed");
@@ -60,7 +87,9 @@ const TaskList = ({ tasks }) => {
 
   return (
     
-    <div className="container">
+   
+
+    <div className="container_parent">
         <div
           className="order small-box"
           onDragLeave={(e) => onDragLeave(e)}
@@ -73,29 +102,28 @@ const TaskList = ({ tasks }) => {
             <div className="container">
               <div className="drag_column">
                 <div className="drag_row">
-                  <h4>New Orders</h4>
+                  <div className="title_tag"><h4>ToDo</h4><div><b>...</b></div></div>
                   {newOrder.map((task) => (
                     <div
                       className="card"
-                      key={task.name}
+                      key={task.id} 
                       id={task.id}
                       draggable
                       onDragStart={(e) => onDragStart(e)}
                       onDragEnd={(e) => onDragEnd(e)}
                     >
-                      <div className="img">
-                        <img src={task.image} alt="box" />
-                      </div>
+                  
                       <div className="card_right">
-                        <div className="status">{task.status}</div>
+                        <div className={`status ${task.color}`}></div>
                         <div className="days">{task.time}</div>
-                        <div className="time">{task.days}</div>
+                        <div className="message_icon_parent"><div className="time"> &#9776;</div><div>&#9993; {task.days}</div> </div>
                       </div>
                     </div>
                   ))}
                 </div>
               </div>
             </div>
+            <button onClick={() => addCard("New Order")} className="add-card-btn" > + Add a card</button>
           </section>
         </div>
         <div
@@ -110,7 +138,7 @@ const TaskList = ({ tasks }) => {
             <div className="container">
               <div className="drag_column">
                 <div className="drag_row">
-                  <h4>In Progress</h4>
+                  <div className="title_tag"><h4>In Progress</h4><div><b>...</b></div></div>
                   {pending.map((task) => (
                     <div
                       className="card"
@@ -121,18 +149,19 @@ const TaskList = ({ tasks }) => {
                       onDragEnd={(e) => onDragEnd(e)}
                     >
                       <div className="img">
-                        <img src={task.image} alt="box" />
+                
                       </div>
                       <div className="card_right">
-                        <div className="status">{task.status}</div>
+                        <div className={`status ${task.color}`}></div>
                         <div className="days">{task.time}</div>
-                        <div className="time">{task.days}</div>
+                        <div className="message_icon_parent"><div className="time"> &#9776;</div><div>&#9993; {task.days}</div> </div>
                       </div>
                     </div>
                   ))}
                 </div>
               </div>
             </div>
+            <button onClick={() => addCard("In Progress")} className="add-card-btn" > + Add a card</button>
           </section>
         </div>
         <div
@@ -147,29 +176,31 @@ const TaskList = ({ tasks }) => {
             <div className="container">
               <div className="drag_column">
                 <div className="drag_row">
-                  <h4>Waiting for buyer</h4>
+                  <div className="title_tag"><h4> Review</h4><div><b>...</b></div></div>
                   {waiting.map((task) => (
                     <div
                       className="card"
                       key={task.name}
                       id={task.id}
-                      draggable
+                      draggablered
                       onDragStart={(e) => onDragStart(e)}
                       onDragEnd={(e) => onDragEnd(e)}
                     >
                       <div className="img">
-                        <img src={task.image} alt="box" />
+                    
                       </div>
                       <div className="card_right">
-                        <div className="status">{task.status}</div>
+                        <div className={`status ${task.color}`}></div>
                         <div className="days">{task.time}</div>
-                        <div className="time">{task.days}</div>
+
+                        <div className="message_icon_parent"><div className="time"> &#9776;</div><div>&#9993; {task.days}</div> </div>
                       </div>
                     </div>
                   ))}
                 </div>
               </div>
             </div>
+            <button onClick={() => addCard("Delivered")} className="add-card-btn" > + Add a card</button>
           </section>
         </div>
         <div
@@ -184,7 +215,7 @@ const TaskList = ({ tasks }) => {
             <div className="container">
               <div className="drag_column">
                 <div className="drag_row">
-                  <h4>Completed</h4>
+                  <div className="title_tag"><h4>Done</h4><div><b>...</b></div></div>
                   {done.map((task) => (
                     <div
                       className="card"
@@ -195,19 +226,65 @@ const TaskList = ({ tasks }) => {
                       onDragEnd={(e) => onDragEnd(e)}
                     >
                       <div className="img">
-                        <img src={task.image} alt="box" />
+                       
                       </div>
                       <div className="card_right">
-                        <div className="status">{task.status}</div>
+                        <div className={`status ${task.color}`}></div>
                         <div className="days">{task.time}</div>
-                        <div className="time">{task.days}</div>
+                        <div className="message_icon_parent"><div className="time"> &#9776;</div><div>&#9993; {task.days}</div> </div>
                       </div>
                     </div>
                   ))}
                 </div>
               </div>
             </div>
+            <button onClick={() => addCard("Completed")} className="add-card-btn" > + Add a card</button>
           </section>
+        </div>
+        <div>
+           
+        {showForm && (
+        <div className="modal-overlay">
+              <div className="close-button" onClick={() => setShowForm(false)}>
+        <span>&times;</span>
+      </div>
+          <div className="modal">
+      
+            <form onSubmit={handleFormSubmit}>
+              <label>
+                Project:
+                <input
+                  type="text"
+                  value={newCardData.time}
+                  onChange={(e) =>
+                    setNewCardData({ ...newCardData, time: e.target.value })
+                  }
+                  required
+                />
+              </label>
+              <label>
+  Color (optional):
+  <select
+    value={newCardData.color}
+    onChange={(e) =>
+      setNewCardData({ ...newCardData, color: e.target.value })
+    }
+  >
+    <option value="">Select Color</option>
+    <option value="green">Green</option>
+    <option value="red">Red</option>
+    <option value="blue">Blue</option>
+    <option value="yellow">Yellow</option>
+  </select>
+</label>
+              <button type="submit">Create Card</button>
+              <button onClick={() => setShowForm(false)} className="close-button">
+        Close
+      </button>
+            </form>
+          </div>
+        </div>
+      )}
         </div>
       </div>
   );
